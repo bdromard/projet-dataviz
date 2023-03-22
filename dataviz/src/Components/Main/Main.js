52ae9a4053830c28e71c418ec58762050a4fbfdd
Malena Guallar <malena.guallar@gmail.com>
2023-03-22 15:36:06 +0100

    creation header

---
+++ dataviz/src/Components/Main/Main.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import "./Main.css";
+import axios from "axios";
+
+function Main (){
+
+
+  
+
+  return (
+    <div>
+    
+    </div>
+  )
+
+
+}; 
+ export default Main ; 
