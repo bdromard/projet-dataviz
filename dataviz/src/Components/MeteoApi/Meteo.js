c759ecffbbeb0357337a6539cb890eba02069f82
Virgil <benjamin.dromard@gmail.com>
2023-03-23 14:37:28 +0100

    dev requête et fonction React meteo

--- dataviz/src/Components/MeteoApi/Meteo.js
+++ dataviz/src/Components/MeteoApi/Meteo.js
@@ -1,11 +1,27 @@
 import React, { useEffect } from "react";
 import "./Meteo.css";
+import axios from "axios";
+
+const divMeteo = ReactDOM.createMeteo(
+document.getElementById('meteo')
+)
 
 function Meteo(){
 
-    return (
-        <div></div>
-    )
-}
+    React.useEffect(() => {
+        async function fetchData() {
+      
+      
+            try {
+                const response = await axios.get(
+                `http://api.weatherapi.com/v1/current.json?key=e00cb81a6b504b01a77131032232303&q=Paris&lang=fr&aqi=yes`
+            );
+            response.data.location.forEach((element) => {
+                const paragraph = document.createElement('p');
+                paragraph.innerHTML = element;
+                divMeteo.appendChild(paragraph)
+            })
+        }
+        }})}
 
 export default Meteo;
