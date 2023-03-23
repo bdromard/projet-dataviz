a89f3921f4e6c82bc9173e0be3ce8ae2df9c59f4
Malena Guallar <malena.guallar@gmail.com>
2023-03-23 12:18:20 +0100

    création sections map + pollution // tentative de changement du background color selon l'heure // création d'un footer

--- dataviz/src/Components/Main/Main.js
+++ dataviz/src/Components/Main/Main.js
@@ -56,18 +56,58 @@ import React, { useEffect } from "react";
 import "./Main.css";
 import axios from "axios";
 
+
+let currentTime = new Date().getHours();
+ 
+
+
 function Main (){
 
-
-  
+ if (currentTime < 12 && currentTime >= 0){
 
   return (
-    <div>
-    
+    <div className="main-container-morning">
+      <section className="pollution">
+        <p>ici la qualité de l'air parisien :&#41;</p>
+      </section>
+      <section className="map">
+      <p>ici la map de Paris</p>
+      </section>
     </div>
-  )
+    )
+
+} else  if (currentTime >= 12 && currentTime <= 18){
+
+  return (
+    <div className="main-container-afternoon">
+        <section className="pollution">
+        <p>ici la qualité de l'air parisien :&#41;</p>
+      </section>
+      <section className="map">
+      <p>ici la map de Paris</p>
+      </section>
+    </div>
+    )
+} else {
+
+  return (
+    <div className="main-container-night">
+        <section className="pollution">
+        <p>ici la qualité de l'air parisien :&#41;</p>
+      </section>
+      <section className="map">
+      <p>ici la map de Paris</p>
+      </section>
+    </div>
+    )
+}
+}
 
 
+<<<<<<< HEAD
 }; 
  export default Main ; 
 >>>>>>> 52ae9a40 (creation header)
+=======
+export default Main ; 
+>>>>>>> 8ded7d4a (création sections map + pollution // tentative de changement du background color selon l'heure // création d'un footer)
