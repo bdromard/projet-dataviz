6530feef88806f841c3f6d4e99d177280c50fec7
Malena Guallar <malena.guallar@gmail.com>
2023-03-27 10:22:36 +0200

    requetes

--- dataviz/src/Components/Map/Map.js
+++ dataviz/src/Components/Map/Map.js
@@ -2,18 +2,40 @@ import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import "./Map.css";
+import axios from "axios"; 
+import espacesData from '/Users/malena/ada_groups/Dataviz/projet-dataviz/dataviz/src/Assets/espaces_verts.json' ; 
 
 function Map() {
 
-  return (
-    
-    <MapContainer center={[48.87, 2.35]} zoom={12.2}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      </MapContainer>  
-      ) ; 
-    } 
+  const getData = async () => {
+    axios.get('https://opendata.paris.fr/api/v2/catalog/datasets/espaces_verts/') 
+
+}
+
+    return (
+      
+  
+        <MapContainer center={[48.87, 2.35]} zoom={12.2}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+
+        {/* {getData(espaces => ( */}
+          <Marker 
+          position={[getData.data.records.fields.geom.coordinates]}>
+
+
+          </Marker>
+
+        {/* ))} */}
+
+          </MapContainer>  
+          
+
+          
+      )
+
+    }
 
     export default Map ; 
