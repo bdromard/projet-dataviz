406bde35aa4617fd794d0f493e745ff1fed649b8
Djeneba Soumare <djenebasoum98@icloud.com>
2023-03-27 14:06:03 +0200

    Map affichage un élément via Api ok ok

--- dataviz/src/Components/Map/Map.js
+++ dataviz/src/Components/Map/Map.js
@@ -46,3 +46,43 @@
 ////////////////////////////////////////////////////////////////
 
 
+import React, { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import "./Map.css";
+import axios from "axios";
+
+function Map() {
+  const [position, setPosition] = useState(null);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const resp = await axios.get(
+        `https://opendata.paris.fr/api/v2/catalog/datasets/espaces_verts/records?refine=adresse_codepostal:75010`
+      );
+      const positionR = [
+        resp.data.records[0].record.fields.geom.geometry.coordinates[0][0][1],
+        resp.data.records[0].record.fields.geom.geometry.coordinates[0][0][0],
+      ];
+      setPosition(positionR);
+    }
+    fetchData();
+    // &refine=nsq_espace_vert:10297
+  }, []);
+  return (
+    <div>
+    {position &&(
+    <MapContainer center={[48.87, 2.35]} zoom={12.2}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      <Marker position={position}></Marker>
+    </MapContainer>
+  )};
+  </div>
+  )
+}
+
+export default Map;
