bdaca5ecf8d0119d22016179e23397caba963b36
Virgil <benjamin.dromard@gmail.com>
2023-03-27 17:33:34 +0200

    sauvegarde travail en cours avec commentaires

--- dataviz/src/Components/Map/Map.js
+++ dataviz/src/Components/Map/Map.js
@@ -1,4 +1,4 @@
-
+// Importation des différentes librairies utilisées pour le rendu de la carte : React, Leaflet, Axios.
 
 import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, Polygon, TileLayer } from "react-leaflet";
@@ -6,25 +6,38 @@ import { Icon } from "leaflet";
 import "./Map.css";
 import axios from "axios";
 
+// Définition d'une couleur pour le dessin des polygones représentant les espaces verts du 75010.
 const redOptions = { color: 'red' }
 
+// Function Map pour le rendu de la map.
 function Map() {
+
+    //Définition d'une constante pour la position à utiliser pour le dessin des espaces verts ; utilisation de useState.
   const [position, setPosition] = useState([]);
 
+    //Fonction asynchrone pour la récupération des données de la mairie de Paris.
   React.useEffect(() => {
     async function fetchData() {
+
+        // Réponse par Axios pour recevoir les données des espaces verts du 75010.
       const resp = await axios.get(
         `https://opendata.paris.fr/api/v2/catalog/datasets/espaces_verts/records?refine=adresse_codepostal:75010`
       );
+      // Coordonnées de test pour la création d'un marqueur. Peut-être à supprimer.
       const positionR = [
         resp.data.records[0].record.fields.geom.geometry.coordinates[0][0][1],
         resp.data.records[0].record.fields.geom.geometry.coordinates[0][0][0],
       ];
+
+      // Travail en cours : boucle forEach pour dessiner un polygone par espace vert. 
       const records = resp.data.records
       records.forEach(record => {
         console.log(record)
         const polygon = resp.data.records[0].record.fields.geom.geometry.coordinates
     
+        // Constitution d'un tableau, qui recevra d'autres tableaux contenants les coordonnées de l'API de la mairie
+        // de Paris. On doit invertir les données de l'API pour chaque tableau, reçues en latitude/longitude ; Leaflet
+        // a besoin de données en longitude/latitude.
       const bigTableau = []
       const polygonReversed = []
       for(let i=0 ; i < polygon.length ; i++){
@@ -40,6 +53,7 @@ function Map() {
     fetchData();
   }, []);
   return (
+    // Rendu de la map sur la page et dessin du polygone sur la carte.
     <div>
     {position &&(
     <MapContainer center={[48.87, 2.35]} zoom={12.2}>
