9afd22df54f692367445798af99ab84a993f52ec
Djeneba Soumare <djenebasoum98@icloud.com>
2023-03-27 14:05:10 +0200

    Map affichage un élément via Api ok

--- dataviz/src/Components/Map/Map.js
+++ dataviz/src/Components/Map/Map.js
@@ -1,79 +1,48 @@
-import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
-import "./Map.css";
-import axios from "axios"; 
-import espacesData from '/Users/malena/ada_groups/Dataviz/projet-dataviz/dataviz/src/Assets/espaces_verts.json' ; 
+// import React, { useEffect, useState } from "react";
+// import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+// import { Icon } from "leaflet";
+// import "./Map.css";
+// import axios from "axios";
+
+// function Map() {
+//   const [positions, setPositions] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       const resp = await axios.get(
+//         `https://opendata.paris.fr/api/v2/catalog/datasets/espaces_verts/records?refine=adresse_codepostal:75010`
+//       );
+//       console.log(resp);
+//       const positionRs = resp.data.records.map((record) => [
+//         record.fields.geom.geometry.coordinates[0][0][1],
+//         record.fields.geom.geometry.coordinates[0][0][0],
+//       ]);
+      
+//       setPositions(positionRs);
+//     }
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div>
+//       <MapContainer center={[48.87, 2.35]} zoom={12.2}>
+//         <TileLayer
+//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//         />
+
+//         {positions.map((position, index) => (
+//           <Marker key={index} position={position}>
+//             <Popup>{`Position ${index + 1}`}</Popup>
+//           </Marker>
+//         ))}
+//       </MapContainer>
+//     </div>
+//   );
+// }
+
+// export default Map;
+
+////////////////////////////////////////////////////////////////
 
 
-function Map() {
-  
-//   axios.get('https://opendata.paris.fr/api/v2/catalog/datasets/espaces_verts/records?refine=adresse_codepostal:75010&select=geom')
-//   .then(resp => {
-
-//     console.log(resp.data);
-// });
-
-React.useEffect(() => {
-  async function fetchData() {
-    const resp = await axios.get(
-      `https://opendata.paris.fr/api/v2/catalog/datasets/espaces_verts/records?refine=adresse_codepostal:75010&refine=nsq_espace_vert:10297`
-    )
-    .then((resp) => {
-      const positionR = [resp.data.records[0].record.fields.geom.geometry.coordinates[0][0][1],
-      resp.data.records[0].record.fields.geom.geometry.coordinates[0][0][0]];
-      console.log(positionR);
-      <MapContainer center={[48.87, 2.35]} zoom={12.2}>
-       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-
-      <Marker
-      position = {positionR
-      }>
-      </Marker>
-
-      </MapContainer> 
-
-  })
-
-
-    // try {
-    //   const resp = await axios.get(
-    //     `https://opendata.paris.fr/api/v2/catalog/datasets/espaces_verts/records?refine=adresse_codepostal:75010&refine=nsq_espace_vert:10297`
-    //   );
-    //   const positionR = [resp.data.records[0].record.fields.geom.geometry.coordinates[0][0][1],
-    //   resp.data.records[0].record.fields.geom.geometry.coordinates[0][0][0]];
-    //   // console.log(resp.data.records[0].record.fields.geom.geometry.coordinates[0][0]);
-        
-    //   return (
-       
-    //   <MapContainer center={[48.87, 2.35]} zoom={12.2}>
-    //    <TileLayer
-    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    //     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    //   />
-
-    //   <Marker
-    //   position = {positionR
-    //   }>
-    //   </Marker>
-
-    //   </MapContainer> )
-
-    // }
-    
-    // catch(err) {
-    //   console.log(err);
-    // }
-
-  
-  }fetchData()
-})}
-  
-
-
-    export default Map ; 
-
-    // URL API Open Data Paris pour les espaces verts du 75010 : https://opendata.paris.fr/api/v2/catalog/datasets/espaces_verts/records?refine=adresse_codepostal:75010
