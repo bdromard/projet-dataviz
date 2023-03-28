ac4e15db5bd9bed93994b9684fb536a14f5e5de5
Virgil <benjamin.dromard@gmail.com>
2023-03-28 11:00:06 +0200

    sauvegarde WIP meteo API

--- dataviz/src/Components/MeteoApi/Meteo.js
+++ dataviz/src/Components/MeteoApi/Meteo.js
@@ -1,27 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Meteo.css";
 import axios from "axios";
 
-const divMeteo = ReactDOM.createMeteo(
-document.getElementById('meteo')
-)
+// const divMeteo = ReactDOM.createMeteo(
+// document.getElementById('meteo')
+// )
 
 function Meteo(){
 
-    React.useEffect(() => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
         async function fetchData() {
       
-      
-            try {
-                const response = await axios.get(
-                `http://api.weatherapi.com/v1/current.json?key=e00cb81a6b504b01a77131032232303&q=Paris&lang=fr&aqi=yes`
-            );
-            response.data.location.forEach((element) => {
-                const paragraph = document.createElement('p');
-                paragraph.innerHTML = element;
-                divMeteo.appendChild(paragraph)
-            })
+        const response = await axios.get(
+        `http://api.weatherapi.com/v1/current.json?key=e00cb81a6b504b01a77131032232303&q=Paris&lang=fr&aqi=yes`
+        );
+        console.log(response.data)
+        setData(response.data)
         }
-        }})}
+fetchData();
+    }, []);
+}
+return(
+    <div>
+    {data &&(
+    <Meteo>
+
+    </Meteo>
+    )}
+    </div>
+    
+        ) 
 
 export default Meteo;
